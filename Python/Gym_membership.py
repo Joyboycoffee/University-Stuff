67914090c21a3df8f1b13b5bdb55003b4bb72e9c
Gourav Kumar <106906124+Joyboycoffee@users.noreply.github.com>
2026-04-11 09:51:54 +0530

    updates

--- Python/Gym_membership.py
+++ Python/Gym_membership.py
@@ -1,24 +1,25 @@
-membership = 2000
+membership = 3500
 age = int(input("Enter age:"))
 discount = 0
-pre_membership = 10000
 
-choice = input("Is the Couple Married?(y/n):").upper()
+
+choice = input("Are you a couple?(y/n):").upper()
 
 if age < 18:
     discount = 0.60 * membership
 
-elif age > 18 and age < 45:
-    discount = 0.36 * membership
+elif age > 18 and age < 60:
+    discount = 0.40 * membership
 
-elif age > 45:
+elif age > 60:
     discount = 0.25 * membership
 
 if choice == 'Y':
     discount += 0.32 * membership
+   
 
-print("Discount is =",discount)
-print("GYM Membership fee is = ",membership - discount)
-
-# pre membership
-# dynamic discount
+print("Discount is =",discount,"Out of",membership)
+if choice == 'Y':
+    print(f"GYM Membership fee is = {membership - discount} Per Person")
+else:
+    print(f"GYM Membership fee is = {membership - discount}")

eb70166ee74bb578b5f2ff91ebe6a5e9b8464356
Gourav Kumar <106906124+Joyboycoffee@users.noreply.github.com>
2026-05-19 16:47:35 +0530

    Enjoy more!

---
+++ Python/Pakages&Modules/main.py
@@ -0,0 +1,4 @@
+from mypackage.student import Student
+
+s1 = Student("Rahul", 101)
+s1.display()
---
+++ Python/Pakages&Modules/mypackage/student.py
@@ -0,0 +1,8 @@
+class Student:
+    def __init__(self, name, roll):
+        self.name = name
+        self.roll = roll
+
+    def display(self):
+        print("Name:", self.name)
+        print("Roll:", self.roll)
